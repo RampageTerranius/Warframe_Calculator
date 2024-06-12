d76c2628cc5cc0fccb3ecb7806d425123dba76eb
RampageTerranius <rampage_terranius@hotmail.com>
2024-06-12 13:28:11 +1000

    true health calculation added

--- Warframe_Calculator/Warframe.Designer.cs
+++ Warframe_Calculator/Warframe.Designer.cs
@@ -30,25 +30,108 @@
         {
             this.components = new System.ComponentModel.Container();
             this.pnl_ = new System.Windows.Forms.Panel();
+            this.numUpDown_TrueHealth = new System.Windows.Forms.NumericUpDown();
+            this.numUpDown_HealthBonus = new System.Windows.Forms.NumericUpDown();
+            this.numUpDown_HealthPercent = new System.Windows.Forms.NumericUpDown();
+            this.numUpDown_BaseHealth = new System.Windows.Forms.NumericUpDown();
+            this.lbl_TrueHealth = new System.Windows.Forms.Label();
+            this.lbl_RawHealthBonus = new System.Windows.Forms.Label();
+            this.lbl_HealthPercent = new System.Windows.Forms.Label();
             this.lbl_bHealth = new System.Windows.Forms.Label();
-            this.tbox_bHealth = new System.Windows.Forms.TextBox();
             this.lbl_Health = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.pnl_.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown_TrueHealth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown_HealthBonus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown_HealthPercent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown_BaseHealth)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnl_
             // 
+            this.pnl_.Controls.Add(this.numUpDown_TrueHealth);
+            this.pnl_.Controls.Add(this.numUpDown_HealthBonus);
+            this.pnl_.Controls.Add(this.numUpDown_HealthPercent);
+            this.pnl_.Controls.Add(this.numUpDown_BaseHealth);
+            this.pnl_.Controls.Add(this.lbl_TrueHealth);
+            this.pnl_.Controls.Add(this.lbl_RawHealthBonus);
+            this.pnl_.Controls.Add(this.lbl_HealthPercent);
             this.pnl_.Controls.Add(this.lbl_bHealth);
-            this.pnl_.Controls.Add(this.tbox_bHealth);
             this.pnl_.Controls.Add(this.lbl_Health);
             this.pnl_.Location = new System.Drawing.Point(12, 27);
             this.pnl_.Name = "pnl_";
-            this.pnl_.Size = new System.Drawing.Size(200, 304);
+            this.pnl_.Size = new System.Drawing.Size(200, 224);
             this.pnl_.TabIndex = 0;
+            // 
+            // numUpDown_TrueHealth
+            // 
+            this.numUpDown_TrueHealth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numUpDown_TrueHealth.Enabled = false;
+            this.numUpDown_TrueHealth.Location = new System.Drawing.Point(3, 187);
+            this.numUpDown_TrueHealth.Name = "numUpDown_TrueHealth";
+            this.numUpDown_TrueHealth.Size = new System.Drawing.Size(194, 23);
+            this.numUpDown_TrueHealth.TabIndex = 10;
+            // 
+            // numUpDown_HealthBonus
+            // 
+            this.numUpDown_HealthBonus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numUpDown_HealthBonus.Location = new System.Drawing.Point(3, 143);
+            this.numUpDown_HealthBonus.Name = "numUpDown_HealthBonus";
+            this.numUpDown_HealthBonus.Size = new System.Drawing.Size(194, 23);
+            this.numUpDown_HealthBonus.TabIndex = 9;
+            this.numUpDown_HealthBonus.ValueChanged += new System.EventHandler(this.HelathBonus_ValueChanged);
+            // 
+            // numUpDown_HealthPercent
+            // 
+            this.numUpDown_HealthPercent.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numUpDown_HealthPercent.Location = new System.Drawing.Point(3, 99);
+            this.numUpDown_HealthPercent.Name = "numUpDown_HealthPercent";
+            this.numUpDown_HealthPercent.Size = new System.Drawing.Size(194, 23);
+            this.numUpDown_HealthPercent.TabIndex = 8;
+            this.numUpDown_HealthPercent.ValueChanged += new System.EventHandler(this.HealthPercent_ValueChanged);
+            // 
+            // numUpDown_BaseHealth
+            // 
+            this.numUpDown_BaseHealth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numUpDown_BaseHealth.Location = new System.Drawing.Point(3, 55);
+            this.numUpDown_BaseHealth.Name = "numUpDown_BaseHealth";
+            this.numUpDown_BaseHealth.Size = new System.Drawing.Size(194, 23);
+            this.numUpDown_BaseHealth.TabIndex = 2;
+            this.numUpDown_BaseHealth.ValueChanged += new System.EventHandler(this.BaseHealth_ValueChanged);
+            // 
+            // lbl_TrueHealth
+            // 
+            this.lbl_TrueHealth.AutoSize = true;
+            this.lbl_TrueHealth.Location = new System.Drawing.Point(3, 169);
+            this.lbl_TrueHealth.Name = "lbl_TrueHealth";
+            this.lbl_TrueHealth.Size = new System.Drawing.Size(65, 15);
+            this.lbl_TrueHealth.TabIndex = 7;
+            this.lbl_TrueHealth.Text = "True health";
+            // 
+            // lbl_RawHealthBonus
+            // 
+            this.lbl_RawHealthBonus.AutoSize = true;
+            this.lbl_RawHealthBonus.Location = new System.Drawing.Point(3, 125);
+            this.lbl_RawHealthBonus.Name = "lbl_RawHealthBonus";
+            this.lbl_RawHealthBonus.Size = new System.Drawing.Size(101, 15);
+            this.lbl_RawHealthBonus.TabIndex = 5;
+            this.lbl_RawHealthBonus.Text = "Raw health bonus";
+            // 
+            // lbl_HealthPercent
+            // 
+            this.lbl_HealthPercent.AutoSize = true;
+            this.lbl_HealthPercent.Location = new System.Drawing.Point(3, 81);
+            this.lbl_HealthPercent.Name = "lbl_HealthPercent";
+            this.lbl_HealthPercent.Size = new System.Drawing.Size(101, 15);
+            this.lbl_HealthPercent.TabIndex = 4;
+            this.lbl_HealthPercent.Text = "Health % increase";
             // 
             // lbl_bHealth
             // 
@@ -58,13 +141,6 @@
             this.lbl_bHealth.Size = new System.Drawing.Size(67, 15);
             this.lbl_bHealth.TabIndex = 2;
             this.lbl_bHealth.Text = "Base health";
-            // 
-            // tbox_bHealth
-            // 
-            this.tbox_bHealth.Location = new System.Drawing.Point(3, 55);
-            this.tbox_bHealth.Name = "tbox_bHealth";
-            this.tbox_bHealth.Size = new System.Drawing.Size(194, 23);
-            this.tbox_bHealth.TabIndex = 1;
             // 
             // lbl_Health
             // 
@@ -91,7 +167,7 @@
             this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
             this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
             this.menuToolStripMenuItem.Text = "Menu";
-            this.menuToolStripMenuItem.Click += new System.EventHandler(this.menuItem_Click);
+            this.menuToolStripMenuItem.Click += new System.EventHandler(this.MenuItem_Click);
             // 
             // contextMenuStrip1
             // 
@@ -102,13 +178,17 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 280);
             this.Controls.Add(this.pnl_);
             this.Controls.Add(this.menuStrip1);
             this.Name = "Warframe";
             this.Text = "Warframe";
             this.pnl_.ResumeLayout(false);
             this.pnl_.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown_TrueHealth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown_HealthBonus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown_HealthPercent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown_BaseHealth)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -119,11 +199,17 @@
         #endregion
 
         private Panel pnl_;
-        private TextBox tbox_bHealth;
         private Label lbl_Health;
         private Label lbl_bHealth;
         private MenuStrip menuStrip1;
         private ToolStripMenuItem menuToolStripMenuItem;
         private ContextMenuStrip contextMenuStrip1;
+        private Label lbl_RawHealthBonus;
+        private Label lbl_HealthPercent;
+        private Label lbl_TrueHealth;
+        private NumericUpDown numUpDown_TrueHealth;
+        private NumericUpDown numUpDown_HealthBonus;
+        private NumericUpDown numUpDown_HealthPercent;
+        private NumericUpDown numUpDown_BaseHealth;
     }
 }
--- Warframe_Calculator/Warframe.cs
+++ Warframe_Calculator/Warframe.cs
@@ -17,16 +17,23 @@ namespace Warframe_Calculator
             InitializeComponent();
 
             this.FormClosing += MenuForm_FormClosing;
+
+            // Set the maximum allowed nubmer to the highest possible.
+            numUpDown_BaseHealth.Maximum = Decimal.MaxValue;
+            numUpDown_HealthBonus.Maximum = Decimal.MaxValue;
+            numUpDown_HealthPercent.Maximum = Decimal.MaxValue;
+            numUpDown_TrueHealth.Maximum = Decimal.MaxValue;
         }
 
-        private void menuItem_Click(object sender, EventArgs e)
+        private void MenuItem_Click(object sender, EventArgs e)
         {
+            // Push across to MenuForm_FormClosing
             this.Close();
         }
 
         private void MenuForm_FormClosing(object? sender, FormClosingEventArgs e)
         {
-            // In the situatino the user attempts to clsoe the program move back to the menu instead.
+            // In the situation the user attempts to close the program move back to the menu instead.
             e.Cancel = true;
 
             using (CustomApplicationContext appContext = new CustomApplicationContext())
@@ -35,6 +42,32 @@ namespace Warframe_Calculator
             }
 
             this.Hide();
+        }
+
+        private void CalculateEHP()
+        {
+            // Calculation for effective armor is base armor * (1 + (armor percentage / 100)) + static bonuses.           
+            // Calculation for true health is base health * (1 + (health percentage / 100)) + static bonuses.
+
+            // Static bonuses includes arcanes (guardian, blessing etc...) and archon shards, these do not add to base and are a flat increase.
+            decimal trueHP = numUpDown_BaseHealth.Value * (1 + (numUpDown_HealthPercent.Value / 100)) + numUpDown_HealthBonus.Value;
+            numUpDown_TrueHealth.Value = trueHP;
+        }
+
+        // Recalculate any time values are changed.
+        private void BaseHealth_ValueChanged(object sender, EventArgs e)
+        {
+            CalculateEHP();
+        }
+
+        private void HealthPercent_ValueChanged(object sender, EventArgs e)
+        {
+            CalculateEHP();
+        }
+
+        private void HelathBonus_ValueChanged(object sender, EventArgs e)
+        {
+            CalculateEHP();
         }
     }
 }
